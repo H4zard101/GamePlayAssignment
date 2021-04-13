277316f242e1f621ed276ce28621e5c1a570ad8a
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2021-04-13 11:52:38 +0100

    some animations(buttons and doors are done)
    added some animations and fixed some other things

---
+++ GamePlayAssignment/Assets/TriggerButtonPressed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerButtonPressed : MonoBehaviour
+{
+    public GameObject Text;
+    public Animator anim;
+    public Animator anim2;
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.Joystick1Button2))
+            {
+                anim.SetBool("trigger", true);
+                anim2.SetBool("triggered", true);
+            }
+            Text.SetActive(true);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Text.SetActive(false);
+        }
+    }
+
+
+}
---
+++ GamePlayAssignment/Assets/openTheSpeedDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openTheSpeedDoor : MonoBehaviour
+{
+
+    public Animator anim;
+    public Transform Door;
+    public float Speed = 5.0F;
+    // Start is called before the first frame update
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Door Move Up");
+            anim.SetBool("Open", true);
+            anim.SetBool("Close", false);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Door Move Down");
+        anim.SetBool("Close", false);
+        anim.SetBool("Open", false);
+        anim.SetBool("StopAnimation", true);
+    }
+}
