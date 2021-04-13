2ddb62dba3fefbf2de3f70624834119a9ecaedc8
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2021-04-13 23:55:05 +0100

    added some platforms and that, still buggy platforms
    i think i know a fix

---
+++ GamePlayAssignment/Assets/Scripts/TiltPlatformBack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltPlatformBack : MonoBehaviour
+{
+    public float speed = 5.0F;
+    public float returnspeed = -5.0F;
+    public Transform origintransform;
+    public GameObject platform;
+
+
+    private void Start()
+    {
+        platform.transform.Rotate(0, 0, 0);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            platform.transform.Rotate(Vector3.left * speed * Time.deltaTime);
+        }
+
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit tilt");
+        platform.transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ GamePlayAssignment/Assets/Scripts/TiltPlatformForward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltPlatformForward : MonoBehaviour
+{
+    public float speed = 5.0F;
+
+    public GameObject platform;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            platform.transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        }
+
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        Debug.Log("exit tilt");
+    }
+}
