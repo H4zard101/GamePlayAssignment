88d8d0be9b0b7b69c857cbeccab4ffb212a61d80
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2021-04-26 21:22:10 +0100

    added a lot of UI

---
+++ GamePlayAssignment/Assets/LevelSelectMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectMenu : MonoBehaviour
+{
+
+    public void level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void level3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
